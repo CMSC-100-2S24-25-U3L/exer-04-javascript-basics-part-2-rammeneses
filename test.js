bfcd2c6a1a2a3ddfc74e1a878e7ba1061ed87df8
Raphael Andrei M. Meneses <rmmeneses@up.edu.ph>
2025-02-26 12:36:07 +0800

    Feat: imported addAccount from index.js; added some test cases, including from the exer specs, into test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+// Importing of addAccount
+import addAccount from './index.js';
+
+// addAccount(["no"])
+addAccount(["no", "nah", "nn", 1,2], true)
+addAccount(["no", "", "test@g.com", 1], true)
+addAccount(["", "nah", "nn", 1], true)
+addAccount(["no", "nah", "", 1], true)
+addAccount(["no", "nah", "test@g.co", 122], true)
+// addAccount([])
+addAccount(["Alan", "Turing", "aturing@w3c.com", 58]);
+addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25])
+addAccount(["Ted", "Nelson", "ted.@w3c.com", 43])
